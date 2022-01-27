ce29f797916549f8a9363cc3867ce689bd7d9635
hilmidemirtas <91216830+hilmidemirtas@users.noreply.github.com>
2022-01-27 20:35:13 +0300

    Update and rename 1_day1..js to 1_day1_exercise_commend..js

---
+++ 30-Days-Of-JavaScript-Exercise/Day1/1_day1_exercise_commend..js
@@ -0,0 +1,14 @@
+//this is my first comment and 
+//comments make code readable
+//I can explain something here for who reads my code
+//Thank you for your visit
+
+
+let firstName = 'Hilmi';
+let lastName= 'Demirtaş';
+console.log('Hello its',' ',firstName,' ',lastName)
+
+/* I
+  really
+  like to
+  comment  */

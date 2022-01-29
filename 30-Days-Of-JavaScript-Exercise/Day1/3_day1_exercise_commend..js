314b79572eb16fa22f303af96a7a0e27bf9b588b
hilmidemirtas <91216830+hilmidemirtas@users.noreply.github.com>
2022-01-29 16:29:20 +0300

    Create 3_day1_exercise_commend..js

---
+++ 30-Days-Of-JavaScript-Exercise/Day1/3_day1_exercise_commend..js
@@ -0,0 +1,13 @@
+/* an make
+code readable,
+easy to
+reuse and informative */
+
+console.log (typeof 5); //number
+console.log(typeof 'Hilmi'); //string
+console.log(typeof true); //boomelan
+console.log(typeof null); //object type
+console.log(typeof undefined);
+
+/* here is js types
+and homework */

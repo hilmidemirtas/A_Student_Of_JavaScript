452d55e134869ea5fb4d81d427c4cc09c154f3c1
hilmidemirtas <91216830+hilmidemirtas@users.noreply.github.com>
2022-01-29 23:21:04 +0300

    Create 5_day1_exercise_typeof_operator.js

---
+++ 30-Days-Of-JavaScript-Exercise/Day1/5_day1_exercise_typeof_operator.js
@@ -0,0 +1,13 @@
+/* Create datatypes.js file and use the JavaScript typeof operator to check different data types.
+Check the data type of each variable */
+
+
+
+let firstName = 'hilmi'
+let lastName = 'demirtaş'
+let country = 'Turkey'
+let city = 'izmir'
+let age = 29
+let isMarried = false
+console.log(typeof firstName,typeof  lastName,typeof country,typeof city, typeof age,typeof isMarried) 
+              >string           >string          >string      >string      >number       >boolean

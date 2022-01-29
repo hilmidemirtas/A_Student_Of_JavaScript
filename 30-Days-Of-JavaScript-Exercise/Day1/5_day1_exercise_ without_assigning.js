3ee6d1fe1547fcff41d532eabc94ede71762c063
hilmidemirtas <91216830+hilmidemirtas@users.noreply.github.com>
2022-01-29 23:28:35 +0300

    Create 5_day1_exercise_ without_assigning.js

---
+++ 30-Days-Of-JavaScript-Exercise/Day1/5_day1_exercise_ without_assigning.js
@@ -0,0 +1,7 @@
+//Declare four variables without assigning values
+
+let city // this value can change
+let country // this value can change
+let love  // this value can change
+const homeland // this value can't change
+

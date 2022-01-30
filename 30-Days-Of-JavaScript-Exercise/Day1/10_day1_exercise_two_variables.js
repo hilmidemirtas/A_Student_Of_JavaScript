ccca5a4b0c8dbd00c1fbadcf84b75b87f5184385
hilmidemirtas <91216830+hilmidemirtas@users.noreply.github.com>
2022-01-30 18:45:40 +0300

    Create 10_day1_exercise_two_variables.js

---
+++ 30-Days-Of-JavaScript-Exercise/Day1/10_day1_exercise_two_variables.js
@@ -0,0 +1,12 @@
+/*
+Exercise 10:
+Declare two variables myAge and yourAge and
+assign them initial values and log to the browser console
+*/
+
+let myAge = 28, 
+    yourAge = 'x'
+
+console.log("I am " , myAge, "years old.", "You are", yourAge, "years old.")
+
+I am  28 years old. You are x years old. //results

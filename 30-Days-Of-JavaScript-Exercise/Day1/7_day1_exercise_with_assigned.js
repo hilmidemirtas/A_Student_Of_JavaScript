91bdaf839d29152c9546eea8869b784f80360852
hilmidemirtas <91216830+hilmidemirtas@users.noreply.github.com>
2022-01-30 18:14:45 +0300

    Create 7_day1_exercise_with_assigned.js

---
+++ 30-Days-Of-JavaScript-Exercise/Day1/7_day1_exercise_with_assigned.js
@@ -0,0 +1,28 @@
+/* 
+exercise 7: Declare four variables with assigned values
+*/
+
+
+//values
+let city = "izmir"
+let country = "Turkey"
+let love = null
+let age = 28
+const homeland = "Gaziantep"
+// Values
+
+//console
+console.log(city, country, love, age, homeland)
+//console
+
+//results
+>izmir Turkey null 28 Gaziantep
+//results
+
+//Values typeof
+console.log(typeof city, typeof country, typeof love, typeof age, typeof homeland)
+//Values typeof
+
+//Values typeof results
+>string string object number string //typeof values
+//Values typeof results

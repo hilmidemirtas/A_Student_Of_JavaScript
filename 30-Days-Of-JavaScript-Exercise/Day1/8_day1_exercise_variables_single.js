dc97e3b4dae81cde4c85c070b1a05d8229ef6acc
hilmidemirtas <91216830+hilmidemirtas@users.noreply.github.com>
2022-01-30 18:35:27 +0300

    Create 8_day1_exercise_variables_single.js

---
+++ 30-Days-Of-JavaScript-Exercise/Day1/8_day1_exercise_variables_single.js
@@ -0,0 +1,13 @@
+/* 
+Exercise 9:
+Declare variables to store your
+first name, last name, marital status, country and age
+in a single line
+*/
+
+let firstName = "hilmi demirtaş", lastName = "demirtaş", maritalStatus = "single", country = "Turkey", age = "28"; //single line
+console.log(firstName, lastName, maritalStatus, country, age) // console
+
+hilmi demirtaş demirtaş single Turkey 28 //results
+
+

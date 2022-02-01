50b47d0228e77a7e33b7e80865cb555068eb482f
hilmidemirtas <91216830+hilmidemirtas@users.noreply.github.com>
2022-02-01 23:23:39 +0300

    Create 03_day2_exercise_length.js

---
+++ 30-Days-Of-JavaScript-Exercise/Day2/03_day2_exercise_length.js
@@ -0,0 +1,19 @@
+/*
+Print the length of the string
+on the browser console using console.log()
+*/
+
+let name = `hilmidemirtas`
+console.log(name.length)
+
+//result
+>13
+//another exercise about the length
+
+let number = `0123456789`
+console.log(number.length)
+
+//result
+>10
+
+//length starts counting from zero. Don't forget it!

1c9a3a9182a5acf717ea73cdfdfd704000a4e5e5
hilmidemirtas <91216830+hilmidemirtas@users.noreply.github.com>
2022-02-01 23:30:36 +0300

    Create 03_day2_exercise_toUpperCase().js

---
+++ 30-Days-Of-JavaScript-Exercise/Day2/03_day2_exercise_toUpperCase().js
@@ -0,0 +1,10 @@
+/* Exercise 4: 
+Change all the string characters
+to capital letters using
+toUpperCase() method */
+
+let name = `hilmi demirtas`
+console.log(name.toUpperCase())
+
+//results
+>HILMI DEMIRTAS

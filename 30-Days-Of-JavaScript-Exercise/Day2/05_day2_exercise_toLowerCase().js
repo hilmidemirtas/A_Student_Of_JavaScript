6e1ef340b8652434da33f9947a487efc23eb3ac9
hilmidemirtas <91216830+hilmidemirtas@users.noreply.github.com>
2022-02-01 23:35:17 +0300

    Create 05_day2_exercise_toLowerCase().js

---
+++ 30-Days-Of-JavaScript-Exercise/Day2/05_day2_exercise_toLowerCase().js
@@ -0,0 +1,11 @@
+/* Exercise 5:
+Change all the string characters
+to lowercase letters using toLowerCase() method
+*/
+
+let name = `HILMI DEMIRTAS`
+console.log(name.toLowerCase())
+
+//result
+
+>hilmi demirtas

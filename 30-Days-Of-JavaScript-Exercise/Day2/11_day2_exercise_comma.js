bbf23bbdaecda71b636373497377424ff4882067
hilmidemirtas <91216830+hilmidemirtas@users.noreply.github.com>
2022-02-03 01:34:42 +0300

    Create 11_day2_exercise_comma.js

---
+++ 30-Days-Of-JavaScript-Exercise/Day2/11_day2_exercise_comma.js
@@ -0,0 +1,10 @@
+/* Exercise 11:
+'Facebook, Google, Microsoft, Apple, IBM, Oracle, Amazon' 
+split the string at the comma and change it to an array.
+*/
+
+let companyName = "Facebook, Google, Microsoft, Apple, IBM, Oracle, Amazon"
+console.log(companyName.split(" "))
+
+//result 
+(7) ['Facebook,', 'Google,', 'Microsoft,', 'Apple,', 'IBM,', 'Oracle,', 'Amazon']

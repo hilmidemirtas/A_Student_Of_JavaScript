bd6053f1213d2a83816c3adc132445d4368fd7e7
hilmidemirtas <91216830+hilmidemirtas@users.noreply.github.com>
2022-02-03 02:02:43 +0300

    Create 14_day2_exercise_replace.js

---
+++ 30-Days-Of-JavaScript-Exercise/Day2/14_day2_exercise_replace.js
@@ -0,0 +1,16 @@
+/* Exercise 14:
+What is the character code of J in
+'30 Days Of JavaScript' string using charCodeAt()
+*/
+
+let course = "30 Days Of JavaScript"
+console.log(course.charAt(11))
+
+//result
+> J         //First we find where J is and we are sure now it is 11
+
+let course = "30 Days Of JavaScript"
+console.log(course.charCodeAt(11))
+
+//result
+>74

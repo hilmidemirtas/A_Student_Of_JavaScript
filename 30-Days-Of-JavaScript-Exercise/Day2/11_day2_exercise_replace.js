1c66f03bd573699added1046027bd0f9261174df
hilmidemirtas <91216830+hilmidemirtas@users.noreply.github.com>
2022-02-03 01:40:46 +0300

    Create 11_day2_exercise_replace.js

---
+++ 30-Days-Of-JavaScript-Exercise/Day2/11_day2_exercise_replace.js
@@ -0,0 +1,10 @@
+/* Exercise 12: 
+Change 30 Days Of JavaScript to
+30 Days Of Python using replace() method.
+*/
+
+let lessonName = "30 Days Of JavaScript"
+console.log(lessonName.replace("JavaScript", "Python"))
+
+//result
+> 30 Days Of Python

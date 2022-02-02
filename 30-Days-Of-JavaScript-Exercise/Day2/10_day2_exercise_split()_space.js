a075a5163dfc836d8a608df1f57696339b158407
hilmidemirtas <91216830+hilmidemirtas@users.noreply.github.com>
2022-02-02 16:33:44 +0300

    Create 10_day2_exercise_split()_space.js

---
+++ 30-Days-Of-JavaScript-Exercise/Day2/10_day2_exercise_split()_space.js
@@ -0,0 +1,17 @@
+/* Exercise 10:
+Split the string 30 Days Of JavaScript,
+at the space using split() method
+*/
+
+let name = "30 Days Of JavaScript"
+console.log(name.split(' '))
+
+//result
+(4) ['30', 'Days', 'Of', 'JavaScript']
+
+//another example
+let mark = "hello! how are you? where are you from? how old are you?"
+console.log(mark.split("?"))
+
+//result
+(4) ['hello! how are you', ' where are you from', ' how old are you', '']

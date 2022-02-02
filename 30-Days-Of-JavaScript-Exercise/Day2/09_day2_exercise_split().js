535fd49602cf04d7a08305bf2317b1d8e01d6d5f
hilmidemirtas <91216830+hilmidemirtas@users.noreply.github.com>
2022-02-02 16:27:01 +0300

    Create 09_day2_exercise_split().js

---
+++ 30-Days-Of-JavaScript-Exercise/Day2/09_day2_exercise_split().js
@@ -0,0 +1,17 @@
+/* Exercise 9:
+Split the string into an array using split() method
+*/
+
+let name = "hilmi demirtas"
+console.log(name.split())
+
+//result
+>['hilmi demirtas']
+
+//into an array
+
+let name = "hilmi demirtas"
+console.log(name.split(""))
+
+//result
+>(14) ['h', 'i', 'l', 'm', 'i', ' ', 'd', 'e', 'm', 'i', 'r', 't', 'a', 's']

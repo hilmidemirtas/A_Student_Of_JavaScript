47d06c7b8e69495d7082c9656497393bbdb56f90
hilmidemirtas <91216830+hilmidemirtas@users.noreply.github.com>
2022-02-02 15:12:52 +0300

    Create 06_day2_exercise_substr().js

---
+++ 30-Days-Of-JavaScript-Exercise/Day2/06_day2_exercise_substr().js
@@ -0,0 +1,29 @@
+/* Exercise 6:
+Cut (slice) out the first word 
+of the string using substr() or substring() method
+*/
+
+let lesson = `JavaScript`
+console.log(lesson.substr(4,9))
+
+//result
+
+>Script
+
+//another example
+
+let lesson = `JavaScript`
+console.log(lesson.substr(0,3))
+
+//result
+
+>Java
+
+//or substring() method
+
+let lesson = `JavaScript`
+console.log(lesson.substring(4))
+
+//result
+
+>Script

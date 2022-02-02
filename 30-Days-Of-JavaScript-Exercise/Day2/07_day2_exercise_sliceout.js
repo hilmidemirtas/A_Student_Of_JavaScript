2eec93333f467e77096a35a7356ff543c57b05ec
hilmidemirtas <91216830+hilmidemirtas@users.noreply.github.com>
2022-02-02 15:16:27 +0300

    Create 07_day2_exercise_sliceout.js

---
+++ 30-Days-Of-JavaScript-Exercise/Day2/07_day2_exercise_sliceout.js
@@ -0,0 +1,11 @@
+/* Exercise 7:
+Slice out the phrase
+Days Of JavaScript from
+30 Days Of JavaScript.
+*/
+
+let lessonName = `30 Days Of JavaScript`
+console.log(lessonName.substr(3))
+
+//result 
+>Days Of JavaScript

62bb0914420cf12b6bd90a1051c95df119d2b1b9
hilmidemirtas <91216830+hilmidemirtas@users.noreply.github.com>
2022-02-03 01:49:38 +0300

    Create 13_day2_exercise_replace.js

---
+++ 30-Days-Of-JavaScript-Exercise/Day2/13_day2_exercise_replace.js
@@ -0,0 +1,10 @@
+/* Exercise 13:
+What is character at index
+15 in '30 Days Of JavaScript' string? Use charAt() method.
+*/
+
+let lesson = "30 Days Of JavaScript" 
+console.log(lesson.charAt(15))
+
+//result
+> S

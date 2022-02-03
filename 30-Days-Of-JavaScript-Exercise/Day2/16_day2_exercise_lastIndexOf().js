3926889a954462c30e01734acf48d00b166d7c0b
hilmidemirtas <91216830+hilmidemirtas@users.noreply.github.com>
2022-02-03 14:07:17 +0300

    Create 16_day2_exercise_lastIndexOf().js

---
+++ 30-Days-Of-JavaScript-Exercise/Day2/16_day2_exercise_lastIndexOf().js
@@ -0,0 +1,10 @@
+/* Exercise 16:
+Use lastIndexOf to determine the position of the last
+occurrence of a in 30 Days Of JavaScript.
+*/
+
+let lesson = "30 Days Of JavaScript"
+console.log(lesson.lastIndexOf("a"))
+
+//result
+>14

d7420a623b8f668330131216e00d630e5d7fe4ec
hilmidemirtas <91216830+hilmidemirtas@users.noreply.github.com>
2022-02-03 14:15:38 +0300

    Create 18_day2_exercise_lastIndexOf()_word.js

---
+++ 30-Days-Of-JavaScript-Exercise/Day2/18_day2_exercise_lastIndexOf()_word.js
@@ -0,0 +1,11 @@
+/* Exercise 18:
+Use lastIndexOf to find the position of the last occurrence
+of the word because in the following sentence:
+'You cannot end a sentence with because because because is a conjunction'
+*/
+
+let word = "You cannot end a sentence with because because because is a conjunction"
+console.log(word.lastIndexOf("because"))
+
+//result
+>47

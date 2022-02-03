3b1ea1774e549d9ce056f40bb67cdd4fa94d77aa
hilmidemirtas <91216830+hilmidemirtas@users.noreply.github.com>
2022-02-03 14:39:38 +0300

    Create 20_day2_exercise_startsWith().js

---
+++ 30-Days-Of-JavaScript-Exercise/Day2/Level1/20_day2_exercise_startsWith().js
@@ -0,0 +1,10 @@
+/* Exercise 20:
+Use startsWith() method with the string
+30 Days Of JavaScript and make the result true
+*/
+
+let name = "30 Days Of JavaScript"
+console.log(name.startsWith("3"))
+
+//result
+>true

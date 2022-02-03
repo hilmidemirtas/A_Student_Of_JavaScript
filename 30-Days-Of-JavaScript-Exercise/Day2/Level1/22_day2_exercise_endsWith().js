aafc201f6e7d1cb39c420cfd54940c67ed642898
hilmidemirtas <91216830+hilmidemirtas@users.noreply.github.com>
2022-02-03 14:50:05 +0300

    Create 22_day2_exercise_endsWith().js

---
+++ 30-Days-Of-JavaScript-Exercise/Day2/Level1/22_day2_exercise_endsWith().js
@@ -0,0 +1,10 @@
+/* Exercise 22:
+Use endsWith() method with the string
+30 Days Of JavaScript and make the result true
+*/
+
+let book = "30 Days Of JavaScript"
+console.log(book.endsWith("t"))
+
+//result
+>true

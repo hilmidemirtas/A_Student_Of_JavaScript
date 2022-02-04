53791a9c0dd6942ad858d686c87ead988bbb7987
hilmidemirtas <91216830+hilmidemirtas@users.noreply.github.com>
2022-02-04 15:55:17 +0300

    Create 01_day2_using_console.log().js

---
+++ 30-Days-Of-JavaScript-Exercise/Day2/Leve2/01_day2_using_console.log().js
@@ -0,0 +1,10 @@
+/* Exercise 1:
+Using console.log() print out the following statement:
+The quote 'There is no exercise better for the heart than reaching down and lifting people up.' by John Holmes teaches us to help one another.
+*/
+
+let words = `The quote 'There is no exercise better for the heart than reaching down and lifting people up.' by John Holmes teaches us to help one another.`
+console.log(words)
+
+//result
+>The quote 'There is no exercise better for the heart than reaching down and lifting people up.' by John Holmes teaches us to help one another.

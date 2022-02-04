57b50db08bd943781afa0a898c4c431988fc3bb9
hilmidemirtas <91216830+hilmidemirtas@users.noreply.github.com>
2022-02-04 15:59:22 +0300

    Create 02_day2_exercise_using_console.log().js

---
+++ 30-Days-Of-JavaScript-Exercise/Day2/Level02/02_day2_exercise_using_console.log().js
@@ -0,0 +1,10 @@
+/* Exercise 2:
+"Love is not patronizing and charity isn't about pity, it is about love. Charity and love are the same -- with charity you give love, so don't just give money but reach out your hand instead."
+*/
+
+let words = `"Love is not patronizing and charity isn't about pity, it is about love. Charity and love are the same -- with charity you give love, so don't just give money but reach out your hand instead."
+`
+console.log(words)
+
+//result
+>"Love is not patronizing and charity isn't about pity, it is about love. Charity and love are the same -- with charity you give love, so don't just give money but reach out your hand instead."

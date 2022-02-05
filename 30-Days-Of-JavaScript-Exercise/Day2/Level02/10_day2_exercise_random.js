7f78a24d5e82468ab85bc25f48a94dc6b670def5
hilmidemirtas <91216830+hilmidemirtas@users.noreply.github.com>
2022-02-05 22:57:00 +0300

    Create 10_day2_exercise_random.js

---
+++ 30-Days-Of-JavaScript-Exercise/Day2/Level02/10_day2_exercise_random.js
@@ -0,0 +1,6 @@
+/* Exercise 10:
+Access the 'JavaScript' string characters using a random number.
+*/
+
+let name = "javascript"
+console.log(name.charAt(Math.floor(Math.random()* (name.length + 1))))

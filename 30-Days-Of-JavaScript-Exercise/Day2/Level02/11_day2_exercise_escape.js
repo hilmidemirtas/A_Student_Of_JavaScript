64ba30b47f56891cf9e65f09e9707a99644daf17
hilmidemirtas <91216830+hilmidemirtas@users.noreply.github.com>
2022-02-05 23:04:35 +0300

    Create 11_day2_exercise_escape.js

---
+++ 30-Days-Of-JavaScript-Exercise/Day2/Level02/11_day2_exercise_escape.js
@@ -0,0 +1,14 @@
+/* Exercise 11:
+Use console.log() and escape characters to print the following pattern.
+*/
+
+let numbers = `1 1 1 1 1\n2 1 2 4 8\n3 1 3 9 27\n4 1 4 16 64\n5 1 5 25 125`
+console.log(numbers)
+
+//result
+
+1 1 1 1 1
+2 1 2 4 8
+3 1 3 9 27
+4 1 4 16 64
+5 1 5 25 125

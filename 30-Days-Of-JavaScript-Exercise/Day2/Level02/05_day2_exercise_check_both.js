1fb3f11cba4de399463126fd5248020e453ebb04
hilmidemirtas <91216830+hilmidemirtas@users.noreply.github.com>
2022-02-05 12:55:29 +0300

    Create 05_day2_exercise_check_both.js

---
+++ 30-Days-Of-JavaScript-Exercise/Day2/Level02/05_day2_exercise_check_both.js
@@ -0,0 +1,14 @@
+/* Exercise 5:
+Check if 'on' is found in both python and jargon
+*/
+
+let name = `python`
+let word = `jargon`
+
+console.log(name.includes(`on`))
+
+console.log(word.includes(`on`))
+
+>true
+
+>true

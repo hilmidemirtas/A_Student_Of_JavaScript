02d349b0cd09274108e8c656620696c9dd4d49cb
hilmidemirtas <91216830+hilmidemirtas@users.noreply.github.com>
2022-02-05 12:59:23 +0300

    Create 06_day2_exercise_check_sentence.js

---
+++ 30-Days-Of-JavaScript-Exercise/Day2/Level02/06_day2_exercise_check_sentence.js
@@ -0,0 +1,8 @@
+/* Exercise 6:
+I hope this course is not full of jargon. Check if jargon is in the sentence.
+*/
+
+let name = `I hope this course is not full of jargon. Check if jargon is in the sentence.`
+console.log(name.includes(`jargon`))
+
+>true

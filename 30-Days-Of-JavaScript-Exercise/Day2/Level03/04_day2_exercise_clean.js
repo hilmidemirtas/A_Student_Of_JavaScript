a6202cf3da604b3934433b871485bda7deff9e49
hilmidemirtas <91216830+hilmidemirtas@users.noreply.github.com>
2022-02-08 23:54:08 +0300

    Create 04_day2_exercise_clean.js

---
+++ 30-Days-Of-JavaScript-Exercise/Day2/Level03/04_day2_exercise_clean.js
@@ -0,0 +1,6 @@
+/* Exercise 4:
+"Calculate the total annual income of the person by extracting the numbers from the following text.
+'He earns 5000 euro from salary per month, 10000 euro annual bonus, 15000 euro online courses per month.'"
+*/
+
+

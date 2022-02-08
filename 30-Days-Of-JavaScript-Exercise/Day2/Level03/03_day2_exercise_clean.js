839f119d73df11ae5640422e5013016e58967a8d
hilmidemirtas <91216830+hilmidemirtas@users.noreply.github.com>
2022-02-09 00:02:04 +0300

    Update 03_day2_exercise_clean.js

--- 30-Days-Of-JavaScript-Exercise/Day2/Level03/03_day2_exercise_clean.js
+++ 30-Days-Of-JavaScript-Exercise/Day2/Level03/03_day2_exercise_clean.js
@@ -7,6 +7,6 @@ any ot#her %jo@bs. %Do@es thi%s mo@tiv#ate yo@u to be a tea@cher!? %Th#is 30#Day
 */
 
 const sentence = '%I $am@% a %tea@cher%, &and& I lo%#ve %te@a@ching%;. The@re $is no@th@ing; &as& mo@re rewarding as educa@ting &and& @emp%o@weri@ng peo@ple.;I found tea@ching m%o@re interesting tha@nany ot#her %jo@bs. %Do@es thi%s mo@tiv#ate yo@u to be a tea@cher!? %Th#is 30#Days&OfJavaScript &is al@so $the $resu@lt of &love& of tea&ching'
-console.log(sentence.replace(/[$&#@%]/g, " "))
+console.log(sentence.replace(/[$&#@%]/g, ""))
 
 >I  am   a  tea cher ,  and  I lo  ve  te a ching ;. The re  is no th ing;  as  mo re rewarding as educa ting  and   emp o weri ng peo ple.;I found tea ching m o re interesting tha nany ot her  jo bs.  Do es thi s mo tiv ate yo u to be a tea cher!?  Th is 30 Days OfJavaScript  is al so  the  resu lt of  love  of tea ching

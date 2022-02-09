731836ed68f199b33b2a57f5b3b68555392fd2f1
hilmidemirtas <91216830+hilmidemirtas@users.noreply.github.com>
2022-02-09 23:10:53 +0300

    Create 01_day3_exercise_check.js

---
+++ 30-Days-Of-JavaScript-Exercise/Day_03/Level01/01_day3_exercise_check.js
@@ -0,0 +1,26 @@
+/* Exercise 1:
+Declare firstName, lastName, country, city, age, isMarried, year variable and
+assign value to it and use the typeof operator to check different data types.
+*/
+
+let firstName = `Hilmi`
+let lastName = `Demirtaş`
+let country = `Turkey` 
+let city = `İzmir`
+let age = 29 
+let isMarried = false 
+const year = 2022
+
+console.log(typeof firstName, typeof lastname, typeof country, typeof city, typeof age, typeof isMarried, typeof year)
+>string string string string number boolean number
+
+//or
+
+>>console.log(`${typeof firstName}\n${typeof lastName}\n${typeof country}\n${typeof city}\n${typeof age}\n${typeof isMarried}\n${typeof year}`)
+string
+string
+string
+string
+number
+boolean
+number

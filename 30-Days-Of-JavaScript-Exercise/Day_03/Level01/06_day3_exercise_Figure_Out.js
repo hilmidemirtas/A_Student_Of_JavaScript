4ba9176e9afd1dec78abf63415c06fe183ca544b
hilmidemirtas <91216830+hilmidemirtas@users.noreply.github.com>
2022-02-10 22:07:01 +0300

    Create 06_day3_exercise_Figure_Out.js

---
+++ 30-Days-Of-JavaScript-Exercise/Day_03/Level01/06_day3_exercise_Figure_Out.js
@@ -0,0 +1,58 @@
+/* Exercise 6:
+Figure out the result of the following expressions first without using console.log(). After you decide the result confirm it by using console.log()
+
+4 > 3 && 10 < 12
+4 > 3 && 10 > 12
+4 > 3 || 10 < 12
+4 > 3 || 10 > 12
+!(4 > 3)
+!(4 < 3)
+!(false)
+!(4 > 3 && 10 < 12)
+!(4 > 3 && 10 > 12)
+!(4 === '4')
+There is no 'on' in both dragon and python
+*/
+
+const math = 4 > 3 && 10 < 12
+console.log(name)
+>true
+
+const math = 4 > 3 && 10 > 12
+console.log(math)
+>false
+
+const math = 4 > 3 || 10 < 12
+console.log(math)
+>true
+
+const math = 4 > 3 || 10 > 12
+console.log(math)
+>true
+
+const math = !(4 > 3)
+console.log(math)
+>false
+
+const math = !(4 < 3)
+console.log(math)
+>true
+
+console.log(!(false))
+>true
+
+const math = !(4 > 3 && 10 < 12)
+console.log(math)
+>false
+
+const math = !(4 > 3 && 10 > 12)
+console.log(math)
+>true
+
+const math = !(4 === '4')
+console.log(math)
+>true
+
+let lang = 'python' && 'dragon'
+console.log(rr.includes(`on`))
+>true

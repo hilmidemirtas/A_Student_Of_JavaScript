ebdd845ae0f2190606c1e358a98d8a1c4d35922c
hilmidemirtas <91216830+hilmidemirtas@users.noreply.github.com>
2022-02-10 18:52:53 +0300

    Create 02_day3_exercise_check_datatype_typeof.js

---
+++ 30-Days-Of-JavaScript-Exercise/Day_03/Level01/02_day3_exercise_check_datatype_typeof.js
@@ -0,0 +1,15 @@
+/* Exercise 2:
+Check if type of '10' is equal to 10
+*/
+
+let x = `10`
+let y = 10
+console.log(x===y)
+
+>false
+
+let x = `10`
+let y = 10
+console.log(typeof x, typeof y)
+
+>string number

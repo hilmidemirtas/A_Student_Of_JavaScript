9ed50580500dfa6a8fe1eb33d3882c703ddb81c0
hilmidemirtas <91216830+hilmidemirtas@users.noreply.github.com>
2022-02-10 22:21:21 +0300

    Create 07_day3_exercise_Date.js

---
+++ 30-Days-Of-JavaScript-Exercise/Day_03/Level01/07_day3_exercise_Date.js
@@ -0,0 +1,28 @@
+/* Exercise 7:
+
+Use the Date object to do the following activities
+
+What is the year today?
+What is the month today as a number?
+What is the date today?
+What is the day today as a number?
+What is the hours now?
+What is the minutes now?
+Find out the numbers of seconds elapsed from January 1, 1970 to now.
+*/
+
+const year =  new Date()
+console.log(year.getFullYear())
+>2022
+
+const month = new Date()
+console.log(month.getMonth()+1)
+>2 (February)
+
+
+
+const today = new Date()
+console.log(today.getDate())
+>10 (Thuesday)
+
+const 

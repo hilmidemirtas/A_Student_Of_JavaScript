86b1e6b6ba5b2bb76ac43de25b261ffbd52e5cd4
hilmidemirtas <91216830+hilmidemirtas@users.noreply.github.com>
2022-02-10 20:21:24 +0300

    Create 04_day3_exercise_Boolean.js

---
+++ 30-Days-Of-JavaScript-Exercise/Day_03/Level01/04_day3_exercise_Boolean.js
@@ -0,0 +1,38 @@
+/* Exercise 4:
+Boolean value is either true or false.
+*/
+
+//true
+
+let x = 7;
+let y = 2;
+console.log(x > y)
+>true
+
+let x = `hilmi`
+let y = `demir`
+console.log(x.length == y.length)
+>true
+
+let x = 8;
+let y = 7;
+console.log( x != y)
+>true
+
+//false
+
+let x = 5
+let y = `5`
+console.log(x != y)
+>false
+
+let x = 27
+
+
+let x = `hilmi`
+let y = `demir`
+console.log(x.length != y.length)
+>false
+
+
+

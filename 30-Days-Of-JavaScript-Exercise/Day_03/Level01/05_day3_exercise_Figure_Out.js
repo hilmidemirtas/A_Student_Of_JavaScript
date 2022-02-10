2758c277844c5e0486544e4bcd312aab82326395
hilmidemirtas <91216830+hilmidemirtas@users.noreply.github.com>
2022-02-10 21:04:10 +0300

    Create 05_day3_exercise_Figure_Out.js

---
+++ 30-Days-Of-JavaScript-Exercise/Day_03/Level01/05_day3_exercise_Figure_Out.js
@@ -0,0 +1,41 @@
+/* Exercise 5:
+Figure out the result of the following comparison expression first without using console.log(). After you decide the result confirm it using console.log()
+
+4 > 3
+4 >= 3
+4 < 3
+4 <= 3
+4 == 4
+4 === 4
+4 != 4
+4 !== 4
+4 != '4'
+4 == '4'
+4 === '4'
+Find the length of python and jargon and make a falsy comparison statement.
+*/
+
+console.log(4 > 3)
+>true
+console.log(4 >= 3)
+>true
+console.log(4 < 3)
+>false
+console.log(4 <= 3)
+>false
+console.log(4 == 4)
+>true
+console.log(4 === 4)
+>true
+console.log(4 != 4)
+>false
+console.log(4 !== 4)
+>false
+console.log(4 != '4')
+>false
+console.log(4 == '4')
+>true
+console.log(4 === '4')
+>false
+console.log('python'.length != 'jargon'.length )
+>false

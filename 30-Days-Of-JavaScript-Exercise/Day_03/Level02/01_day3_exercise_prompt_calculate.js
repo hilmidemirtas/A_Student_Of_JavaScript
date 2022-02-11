c626117baf4469bd692cd29b469e5fb5ad7129ca
hilmidemirtas <91216830+hilmidemirtas@users.noreply.github.com>
2022-02-11 17:41:34 +0300

    Update 01_day3_exercise_prompt_calculate.js

--- 30-Days-Of-JavaScript-Exercise/Day_03/Level02/01_day3_exercise_prompt_calculate.js
+++ 30-Days-Of-JavaScript-Exercise/Day_03/Level02/01_day3_exercise_prompt_calculate.js
@@ -1,12 +1,10 @@
 /* Exercise 1:
-Write a script that prompt the user to enter base and height of the triangle and calculate an area of a triangle (area = 0.5 x b x h).
-Enter side a: 5
-Enter side b: 4
-Enter side c: 3
-The perimeter of the triangle is 12
+Write a script that prompt the user to enter base and height
+of the triangle and calculate an area of a triangle (area = 0.5 x b x h).
+
 */
 
 let a = prompt('Enter base')
 let b = prompt('Enter height')
-let perimeter = (parseInt(a) * parseInt(b) * 0.5 )
-alert(perimeter)
+let area = (parseInt(a) * parseInt(b) * 0.5 )
+alert(`The area is: ${area}`)

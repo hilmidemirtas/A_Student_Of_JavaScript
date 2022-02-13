bc676154cf00359a8aea07bf3a405c8d0c439714
hilmidemirtas <91216830+hilmidemirtas@users.noreply.github.com>
2022-02-13 23:40:43 +0300

    Create 12.js

---
+++ 30-Days-Of-JavaScript-Exercise/Day_03/Level02/12.js
@@ -0,0 +1,6 @@
+//12
+let my = parseInt(prompt('How old am I?'))
+let your = parseInt(prompt('How old are you?'))
+my > your
+? alert(`I am ${my - your} years older than you.`)
+: alert(`You are ${your - my} years older than me`) 

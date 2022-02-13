e946cd9228751217fdc46f54d12500530c34674e
hilmidemirtas <91216830+hilmidemirtas@users.noreply.github.com>
2022-02-13 23:38:37 +0300

    Create 04.js

---
+++ 30-Days-Of-JavaScript-Exercise/Day_03/Level02/04.js
@@ -0,0 +1,7 @@
+//4
+let radius = prompt(`What is the radius?`)
+const pi = 3.14
+let area = (parseInt(radius)**2 * pi)
+let circle = (parseInt(radius)*2*pi)
+alert(`Area is: ${area}`)
+alert(`Circle is: ${circle}`)

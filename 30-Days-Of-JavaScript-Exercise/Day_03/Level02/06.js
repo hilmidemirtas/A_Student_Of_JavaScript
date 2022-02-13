8156ebb38054157e6676902654fa2458f410adcf
hilmidemirtas <91216830+hilmidemirtas@users.noreply.github.com>
2022-02-13 23:39:12 +0300

    Create 06.js

---
+++ 30-Days-Of-JavaScript-Exercise/Day_03/Level02/06.js
@@ -0,0 +1,7 @@
+//6
+let x1 = parseInt(prompt('What is the x1?'))
+let y1 = parseInt(prompt('What is the y1?'))
+let x2 = parseInt(prompt('What is the x2?'))
+let y2 = parseInt(prompt('What is the y2?'))
+let slope = (y2-y1/x2-x1)
+alert('slope is : ' + slope)

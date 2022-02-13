4bb69158ec3eb9c0f949eff9d175b0ca714db725
hilmidemirtas <91216830+hilmidemirtas@users.noreply.github.com>
2022-02-13 23:40:27 +0300

    Create 11.js

---
+++ 30-Days-Of-JavaScript-Exercise/Day_03/Level02/11.js
@@ -0,0 +1,7 @@
+//11
+
+let firstName = prompt('What is your first name?')
+let lastName = prompt('What is your first name?')
+firstName.length > lastName.length
+? alert(`Your first name, ${firstName} is longer than your family name, ${lastName}`)
+: alert(`Your family name, ${lastName} is longer than your first name, ${firstName}`)

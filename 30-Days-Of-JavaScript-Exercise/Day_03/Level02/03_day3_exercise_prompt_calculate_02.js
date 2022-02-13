5216fdac19bdb9cd53daf586dcc3066f39fc6fc8
hilmidemirtas <91216830+hilmidemirtas@users.noreply.github.com>
2022-02-13 17:36:36 +0300

    Update 03_day3_exercise_prompt_calculate_02.js

--- 30-Days-Of-JavaScript-Exercise/Day_03/Level02/03_day3_exercise_prompt_calculate_02.js
+++ 30-Days-Of-JavaScript-Exercise/Day_03/Level02/03_day3_exercise_prompt_calculate_02.js
@@ -77,6 +77,15 @@ my > your
 : alert(`You are ${your - my} years older than me`) 
 
 //13 
+let birth = parseInt(prompt('Please Enter Your Birth Year: '))
+const sss = new Date()
+const years = sss.getFullYear()
+let can = years - birth
+let wait = 18 - can
+can >= 18
+ ? alert(`You are ${can}. You are old enough to drive`)
+ : alert(`You are ${can}. You will be allowed to drive after ${wait} years.`)
 
+//14
 
 

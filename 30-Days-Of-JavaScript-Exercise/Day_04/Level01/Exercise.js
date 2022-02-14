e5dfca07519ea933ce79b829a279ddaa338a7e8e
hilmidemirtas <91216830+hilmidemirtas@users.noreply.github.com>
2022-02-14 18:45:10 +0300

    Update Exercise.js

--- 30-Days-Of-JavaScript-Exercise/Day_04/Level01/Exercise.js
+++ 30-Days-Of-JavaScript-Exercise/Day_04/Level01/Exercise.js
@@ -1,1 +1,26 @@
+//1
+let age = parseInt(prompt('Please Enter Your Age: '))
+if (age >= 18)
+{
+   alert(`You are old enough to drive`)    
+}
+
+else {
+    alert(`You are left with ${18 - age} years to drive`)
+}
+
+//2
+let yourAge = parseInt(prompt('enter your age: '))
+let myAge = 29
+if (yourAge > myAge) {
+alert(`You are ${yourAge - myAge} years older than me.`)
+}
+else if (yourAge == myAge){
+    alert(`we are the same old`)
+}
+else {
+alert(`I am ${myAge - yourAge} years older than you`) }
+
+//3
+
 

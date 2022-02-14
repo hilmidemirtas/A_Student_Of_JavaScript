ebf4712e7bfb6fb821350e1317a7f589157a4366
hilmidemirtas <91216830+hilmidemirtas@users.noreply.github.com>
2022-02-15 02:09:03 +0300

    Update Exercise.js

--- 30-Days-Of-JavaScript-Exercise/Day_04/Level03/Exercise.js
+++ 30-Days-Of-JavaScript-Exercise/Day_04/Level03/Exercise.js
@@ -1,1 +1,34 @@
 //1
+let month = prompt(`Enter a month: `)
+      if (month == 'January') {
+          alert(`{$month} has 31 days`)
+      }
+      else if (month == 'February') {
+          alert(`${month} has 28 days`)
+      }
+      else if (month == 'March') {
+          alert(`${month} has 31 days`)
+      }
+      else if (month == 'May') {
+          alert(`${month} has 31 days`)
+      }
+      else if (month == 'July') {
+          alert(`${month} has 31 days`)
+      }
+      else if (month == 'June') {
+          alert(`${month} has 31 days`)
+      }
+      else if (month == 'August') {
+          alert(`${month} has 31 days`)
+      }
+      else if (month == 'October') {
+          alert(`${month} has 31 days`)
+      }
+      else if (month == 'December') {
+          alert(`${month} has 31 days`)
+      }
+      else {
+          alert(`${month} has 30 days`)
+      }
+
+????????????????????????????????????????????????????

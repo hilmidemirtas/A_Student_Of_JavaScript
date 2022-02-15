f1d003c17ef7c2a235799dd800f61cbdf202bfe8
hilmidemirtas <91216830+hilmidemirtas@users.noreply.github.com>
2022-02-15 15:05:41 +0300

    Create Exercise.js

---
+++ 30-Days-Of-JavaScript-Exercise/Day_05/Leve01/Exercise.js
@@ -0,0 +1,30 @@
+//1
+const firstArr = Array()
+console.log(firstArr)
+//or
+const firstArra = []
+console.log(firstArr)
+
+//2
+
+const number = [1, 2, 3, 4, 5]
+console.log(number)
+
+//3
+const number = [1, 2, 3, 4, 5]
+console.log(number.length) 
+
+//4
+      const mixedDataTypes = [28, 'hilmi',true, { name : 'hilmi'}, ['june', 'july', 'august']]
+      console.log(mixedDataTypes)
+
+//5-10
+const itCompanies = ['facebook', 'google', 'microsoft', 'apple', 'ibm', 'oracle', 'amazon']
+      console.log(itCompanies)
+      console.log(itCompanies.length)
+      console.log(itCompanies[0])
+      console.log(itCompanies[3])
+      console.log(itCompanies[6])
+
+
+//

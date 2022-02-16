0db0b2e233f2d1e3918917f39b6dd4b8cdc58373
hilmidemirtas <91216830+hilmidemirtas@users.noreply.github.com>
2022-02-17 01:29:55 +0300

    Update exercise.js

--- 30-Days-Of-JavaScript-Exercise/Day_05/Level02/exercise.js
+++ 30-Days-Of-JavaScript-Exercise/Day_05/Level02/exercise.js
@@ -1,3 +1,14 @@
 // Exercise Level 2:
 
 //1
+
+//needs documents---not now
+
+//2
+let text =
+'I love teaching and empowering people. I teach HTML, CSS, JS, React, Python.'
+let words = text.split(".").join("").split(",").join("").split(" ")
+console.log(words)
+console.log(words.length)
+ 
+//3

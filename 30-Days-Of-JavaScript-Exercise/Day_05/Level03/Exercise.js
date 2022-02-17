0e299b60f77fc4842db4b4f1989c38f258886c21
hilmidemirtas <91216830+hilmidemirtas@users.noreply.github.com>
2022-02-17 23:30:25 +0300

    Update Exercise.js

--- 30-Days-Of-JavaScript-Exercise/Day_05/Level03/Exercise.js
+++ 30-Days-Of-JavaScript-Exercise/Day_05/Level03/Exercise.js
@@ -1,6 +1,12 @@
 //Level 02
 //Exercise
 
-//1
+//1.1
 const ages = [19, 22, 19, 24, 20, 25, 26, 24, 25, 24]
 console.log(ages.sort())
+
+//1.2
+let a = ages[0]
+let b = ages[1]
+let c = a + b ..........
+console.log(c)

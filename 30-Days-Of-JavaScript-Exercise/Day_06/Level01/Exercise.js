330297fa9db494090a97672afba4e328b88570ac
hilmidemirtas <91216830+hilmidemirtas@users.noreply.github.com>
2022-02-17 23:41:12 +0300

    Update Exercise.js

--- 30-Days-Of-JavaScript-Exercise/Day_06/Level01/Exercise.js
+++ 30-Days-Of-JavaScript-Exercise/Day_06/Level01/Exercise.js
@@ -1,1 +1,25 @@
 //level01
+//Exercise
+//1.i
+for(let i = 0; i <= 10; i++){
+  console.log(i)
+}
+//1.ii
+let i = 0;
+
+while(i<=10) {
+    console.log(i)
+    i++
+}
+//1.iii
+let i = 0
+do {
+    console.log(i)
+    i++
+}
+    while(i <= 10)
+      
+      
+//2
+
+

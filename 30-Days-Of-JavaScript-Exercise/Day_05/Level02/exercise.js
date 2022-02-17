6d2e870bc11b18dcc7f546a3133cc82992633cfa
hilmidemirtas <91216830+hilmidemirtas@users.noreply.github.com>
2022-02-17 22:56:37 +0300

    Update exercise.js

--- 30-Days-Of-JavaScript-Exercise/Day_05/Level02/exercise.js
+++ 30-Days-Of-JavaScript-Exercise/Day_05/Level02/exercise.js
@@ -75,3 +75,8 @@ else {
 }
 
 //6
+const frontEnd = ['HTML', 'CSS', 'JS', 'React', 'Redux']
+const backEnd = ['Node','Express', 'MongoDB']
+const fullStack = frontEnd.concat(backEnd)
+
+console.log(fullStack)

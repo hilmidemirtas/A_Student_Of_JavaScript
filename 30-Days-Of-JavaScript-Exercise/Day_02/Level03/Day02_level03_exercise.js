c6605ea441be79aa5b5f6fad36947ec260ee298f
hilmidemirtas <91216830+hilmidemirtas@users.noreply.github.com>
2022-02-20 18:06:51 +0300

    Create Day02_level03_exercise.js

---
+++ 30-Days-Of-JavaScript-Exercise/Day_02/Level03/Day02_level03_exercise.js
@@ -0,0 +1,46 @@
+/* Exercise 1:
+'Love is the best thing in this world.
+Some found their love and some are still looking for their love.
+' Count the number of word love in this sentence.
+*/
+
+let string = 'Love is the best thing in this world. Some found their love and some are still looking for their love.';
+let x = /love/gi;
+console.log(string.match(x))
+
+>(3) ['Love', 'love', 'love']
+
+
+
+/*Exercise 2:
+Use match() to count the number of all because in the following sentence:
+'You cannot end a sentence with because because because is a conjunction'
+*/
+let string = 'You cannot end a sentence with because because because is a conjunction';
+let x = /because/gi;
+console.log(string.match(x))
+
+>(3) ['because', 'because', 'because']
+
+
+/* Exercise 3: 
+Clean the following text and find the most frequent word
+(hint, use replace and regular expressions).
+ const sentence = '%I $am@% a %tea@cher%, &and& I lo%#ve %te@a@ching%;. The@re $is no@th@ing; &as& mo@re rewarding as educa@ting &and& @emp%o@weri@ng peo@ple.;
+I found tea@ching m%o@re interesting tha@n
+any ot#her %jo@bs. %Do@es thi%s mo@tiv#ate yo@u to be a tea@cher!? %Th#is 30#Days&OfJavaScript &is al@so $the $resu@lt of &love& of tea&ching'
+*/
+
+const sentence = '%I $am@% a %tea@cher%, &and& I lo%#ve %te@a@ching%;. The@re $is no@th@ing; &as& mo@re rewarding as educa@ting &and& @emp%o@weri@ng peo@ple.;I found tea@ching m%o@re interesting tha@nany ot#her %jo@bs. %Do@es thi%s mo@tiv#ate yo@u to be a tea@cher!? %Th#is 30#Days&OfJavaScript &is al@so $the $resu@lt of &love& of tea&ching'
+console.log(sentence.replace(/[$&#@%]/g, ""))
+
+>I  am   a  tea cher ,  and  I lo  ve  te a ching ;. The re  is no th ing;  as  mo re rewarding as educa ting  and   emp o weri ng peo ple.;I found tea ching m o re interesting tha nany ot her  jo bs.  Do es thi s mo tiv ate yo u to be a tea cher!?  Th is 30 Days OfJavaScript  is al so  the  resu lt of  love  of tea ching
+
+
+
+/* Exercise 4:
+"Calculate the total annual income of the person by extracting the numbers from the following text.
+'He earns 5000 euro from salary per month, 10000 euro annual bonus, 15000 euro online courses per month.'"
+*/
+
+let x 

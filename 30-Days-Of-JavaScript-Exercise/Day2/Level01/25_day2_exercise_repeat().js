2411d12180ba418354e23d732df39d3dc5641aef
hilmidemirtas <91216830+hilmidemirtas@users.noreply.github.com>
2022-02-20 18:09:39 +0300

    Delete 30-Days-Of-JavaScript-Exercise/Day2/Level01 directory

--- 30-Days-Of-JavaScript-Exercise/Day2/Level01/25_day2_exercise_repeat().js
+++
@@ -1,9 +0,0 @@
-/* Exercise 25:
-Use repeat() method to print 30 Days Of JavaScript 2 times
-*/
-
-let name = ` 30 Days Of JavaScript`
-console.log(name.repeat(2))
-
-//result
->30 Days Of JavaScript 30 Days Of JavaScript

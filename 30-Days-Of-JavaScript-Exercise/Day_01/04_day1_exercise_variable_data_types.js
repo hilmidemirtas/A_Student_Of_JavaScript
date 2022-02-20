afb39e4e68a52ac08818fca930fb6290479cbe02
hilmidemirtas <91216830+hilmidemirtas@users.noreply.github.com>
2022-02-20 17:06:29 +0300

    Update and rename 30-Days-Of-JavaScript-Exercise/Day1/04_day1_exercise_variable_data_types.js to 30-Days-Of-JavaScript-Exercise/Day_01/04_day1_exercise_variable_data_types.js

--- 30-Days-Of-JavaScript-Exercise/Day_01/04_day1_exercise_variable_data_types.js
+++ 30-Days-Of-JavaScript-Exercise/Day_01/04_day1_exercise_variable_data_types.js
@@ -1,4 +1,4 @@
-/* Create a variable.js file and declare variables and assign
+/*Exercise 4: Create a variable.js file and declare variables and assign
 string, boolean, undefined and null data types */ 
 
 onsole.log (typeof 29); //number

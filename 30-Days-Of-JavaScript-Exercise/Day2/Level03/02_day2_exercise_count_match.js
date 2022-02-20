c894fffcc320766ec0fe8a788436fc84a0eeea8b
hilmidemirtas <91216830+hilmidemirtas@users.noreply.github.com>
2022-02-20 18:04:39 +0300

    Delete 02_day2_exercise_count_match.js

--- 30-Days-Of-JavaScript-Exercise/Day2/Level03/02_day2_exercise_count_match.js
+++
@@ -1,9 +0,0 @@
-/*Exercise 2:
-Use match() to count the number of all because in the following sentence:
-'You cannot end a sentence with because because because is a conjunction'
-*/
-let string = 'You cannot end a sentence with because because because is a conjunction';
-let x = /because/gi;
-console.log(string.match(x))
-
->(3) ['because', 'because', 'because']

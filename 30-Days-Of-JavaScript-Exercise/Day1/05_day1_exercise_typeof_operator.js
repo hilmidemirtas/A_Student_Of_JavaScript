d94f1ffb83e0b87ee2968fbdf67ed69e577f1137
hilmidemirtas <91216830+hilmidemirtas@users.noreply.github.com>
2022-02-20 17:07:00 +0300

    Update 05_day1_exercise_typeof_operator.js

--- 30-Days-Of-JavaScript-Exercise/Day1/05_day1_exercise_typeof_operator.js
+++ 30-Days-Of-JavaScript-Exercise/Day1/05_day1_exercise_typeof_operator.js
@@ -1,4 +1,4 @@
-/* Create datatypes.js file and use the JavaScript typeof operator to check different data types.
+/*Exercise 5: Create datatypes.js file and use the JavaScript typeof operator to check different data types.
 Check the data type of each variable */
 
 

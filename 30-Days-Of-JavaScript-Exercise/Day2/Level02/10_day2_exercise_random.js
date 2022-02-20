e3bc72512cb9bae7494173c01439a133b5690eb2
hilmidemirtas <91216830+hilmidemirtas@users.noreply.github.com>
2022-02-20 18:01:35 +0300

    Delete 10_day2_exercise_random.js

--- 30-Days-Of-JavaScript-Exercise/Day2/Level02/10_day2_exercise_random.js
+++
@@ -1,6 +0,0 @@
-/* Exercise 10:
-Access the 'JavaScript' string characters using a random number.
-*/
-
-let name = "javascript"
-console.log(name.charAt(Math.floor(Math.random()* (name.length + 1))))

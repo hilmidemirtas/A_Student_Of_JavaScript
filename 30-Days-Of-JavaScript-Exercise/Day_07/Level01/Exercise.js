53a13822279e9406f7ff7de1c4e0950d90f09c67
hilmidemirtas <91216830+hilmidemirtas@users.noreply.github.com>
2022-02-21 21:55:56 +0300

    Update Exercise.js

--- 30-Days-Of-JavaScript-Exercise/Day_07/Level01/Exercise.js
+++ 30-Days-Of-JavaScript-Exercise/Day_07/Level01/Exercise.js
@@ -1,3 +1,15 @@
 //level 01
 
 //Exercise 01
+/* 1. Declare a function fullName and it print out your full name.*/
+
+function fullName() { 
+    let firstName = `hilmi`
+    let lastName = ` demirtaş`
+    let fName = firstName + lastName
+    console.log(fName)
+  }
+fullName()
+
+//Exercise 02
+/

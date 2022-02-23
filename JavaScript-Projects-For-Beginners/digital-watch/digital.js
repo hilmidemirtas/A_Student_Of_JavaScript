fff910b7898d2c83ca098e6f32700c9afd10b8bf
hilmidemirtas <91216830+hilmidemirtas@users.noreply.github.com>
2022-02-23 19:17:33 +0300

    Create digital.js

---
+++ JavaScript-Projects-For-Beginners/digital-watch/digital.js
@@ -0,0 +1,10 @@
+function time() {
+    const now = new Date();
+    const saat = now.getHours();
+    const dakika = now.getMinutes();
+    const saniye = now.getSeconds();
+    
+    console.log(`${saat}:${dakika}:${saniye}`)
+
+}
+time()

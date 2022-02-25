d126fb999cf95bbdcafabed8b65d8026a4aa60e0
hilmidemirtas <91216830+hilmidemirtas@users.noreply.github.com>
2022-02-25 03:26:27 +0300

    Create Exercise.js

---
+++ 30-Days-Of-JavaScript-Exercise/Day_08_Objects/Level01/Exercise.js
@@ -0,0 +1,28 @@
+//Exercise 1
+/*1.Create an empty object called dog */
+const dog = {}
+
+
+
+//Exercise 2
+/*2.Print the the dog object on the console*/
+const dog = {}
+console.log(dog)
+
+
+
+//Exercise 3
+/*3.Add name, legs, color, age and bark properties for the dog object. The bark property is a method which return woof woof  */
+const dog = { name:"fox", color:"brown", age:8, bark: function() {return `woof woof`}}
+
+
+//Exercise 4
+/*4.Get name, legs, color, age and bark value from the dog object*/
+const dog = { name:"fox", color:"brown", age:8, bark: function() {
+    return `woof woof`
+}}
+console.log(dog)
+
+
+//Exercise 5
+/*Set new properties the dog object: breed, getDogInfo*/

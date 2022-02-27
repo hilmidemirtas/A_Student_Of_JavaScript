ad8f4b164a3afd848af4daeb6989b216c7308c86
hilmidemirtas <91216830+hilmidemirtas@users.noreply.github.com>
2022-02-27 23:19:33 +0300

    Update Exercise.js
    exerices 1 to 4

--- 30-Days-Of-JavaScript-Exercise/Day_09_Higher_Order_Func/Level01/Exercise.js
+++ 30-Days-Of-JavaScript-Exercise/Day_09_Higher_Order_Func/Level01/Exercise.js
@@ -1,2 +1,30 @@
 //Level01 
-//1.
+
+const countries = ['Finland', 'Sweden', 'Denmark', 'Norway', 'IceLand']
+const names = ['Asabeneh', 'Mathias', 'Elias', 'Brook']
+const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+const products = [
+  { product: 'banana', price: 3 },
+  { product: 'mango', price: 6 },
+  { product: 'potato', price: ' ' },
+  { product: 'avocado', price: 8 },
+  { product: 'coffee', price: 10 },
+  { product: 'tea', price: '' },
+]
+
+//1.Explain the difference between forEach, map, filter, and reduce.
+//you can check documents
+
+//2.Define a call function before you them in forEach, map, filter or reduce.
+
+//3.Use forEach to console.log each country in the countries array.
+
+>countries.forEach((element) => console.log(element))
+
+
+//4.Use forEach to console.log each name in the names array.
+
+>names.forEach((element) => console.log(element))
+
+//5.
+

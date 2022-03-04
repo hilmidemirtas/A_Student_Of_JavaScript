3bc70b24768d82ae5ae48abef1a2e1cd8ade407a
hilmidemirtas <91216830+hilmidemirtas@users.noreply.github.com>
2022-03-04 22:04:06 +0300

    Update Exercise.js
    exercise 9

--- 30-Days-Of-JavaScript-Exercise/Day_09_Higher_Order_Func/Level01/Exercise.js
+++ 30-Days-Of-JavaScript-Exercise/Day_09_Higher_Order_Func/Level01/Exercise.js
@@ -51,7 +51,9 @@ console.log(numbersSquare)
 
 //9.Use map to change to each name to uppercase in the names array
 
-
+const names = ['Asabeneh', 'Mathias', 'Elias', 'Brook'];
+const namesToUpperCase = names.map((name) => name.toUpperCase())
+console.log(namesToUpperCase)
 
 //10.Use map to map the products array to its corresponding prices.
 

5cc61908c00cec61d090b3515b1cdf92d608b592
hilmidemirtas <91216830+hilmidemirtas@users.noreply.github.com>
2022-03-04 22:02:01 +0300

    Update Exercise.js

--- 30-Days-Of-JavaScript-Exercise/Day_09_Higher_Order_Func/Level01/Exercise.js
+++ 30-Days-Of-JavaScript-Exercise/Day_09_Higher_Order_Func/Level01/Exercise.js
@@ -41,9 +41,17 @@ countries.forEach((element) => console.log(element.toUpperCase()))
 
 //7.Use map to create an array of countries length from countries array.
 
+
+
 //8.Use map to create a new array by changing each number to square in the numbers array
 
+const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+const numbersSquare = numbers.map((num) => num * num)
+console.log(numbersSquare)
+
 //9.Use map to change to each name to uppercase in the names array
+
+
 
 //10.Use map to map the products array to its corresponding prices.
 

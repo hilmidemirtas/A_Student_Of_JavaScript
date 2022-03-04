2d8ee29cff1376852f4ec751c4ab63a2eea2c459
hilmidemirtas <91216830+hilmidemirtas@users.noreply.github.com>
2022-03-04 21:53:28 +0300

    Update Exercise.js
    exercise 5

--- 30-Days-Of-JavaScript-Exercise/Day_09_Higher_Order_Func/Level01/Exercise.js
+++ 30-Days-Of-JavaScript-Exercise/Day_09_Higher_Order_Func/Level01/Exercise.js
@@ -28,6 +28,11 @@ const products = [
 
 //5.Use forEach to console.log each number in the numbers array.
 
+let sum = 0;
+const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+numbers.forEach(num => console.log(num))
+console.log(sum)
+
 
 //6.Use map to create a new array by changing each country to uppercase in the countries array.
 

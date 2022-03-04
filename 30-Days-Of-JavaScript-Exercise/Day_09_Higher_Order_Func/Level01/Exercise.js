fd681aeba7ea2c70a739d2e925db5ae950ec9457
hilmidemirtas <91216830+hilmidemirtas@users.noreply.github.com>
2022-03-04 21:55:30 +0300

    Update Exercise.js
    Exercise 6

--- 30-Days-Of-JavaScript-Exercise/Day_09_Higher_Order_Func/Level01/Exercise.js
+++ 30-Days-Of-JavaScript-Exercise/Day_09_Higher_Order_Func/Level01/Exercise.js
@@ -36,6 +36,9 @@ console.log(sum)
 
 //6.Use map to create a new array by changing each country to uppercase in the countries array.
 
+const countries = ['Finland', 'Sweden', 'Denmark', 'Norway', 'IceLand'];
+countries.forEach((element) => console.log(element.toUpperCase()))
+
 //7.Use map to create an array of countries length from countries array.
 
 //8.Use map to create a new array by changing each number to square in the numbers array

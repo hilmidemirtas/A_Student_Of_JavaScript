0f68dbb86cf7c1913dde787ac3682e2b9890e528
hilmidemirtas <91216830+hilmidemirtas@users.noreply.github.com>
2022-03-08 23:37:07 +0300

    Update Exercise.js
    Exercise 2

--- 30-Days-Of-JavaScript-Exercise/Day_10_Set/Level01/Exercise.js
+++ 30-Days-Of-JavaScript-Exercise/Day_10_Set/Level01/Exercise.js
@@ -5,8 +5,12 @@ const a = new Set()
 console.log(a)
 
 //02.Create a set containing 0 to 10 using loop
-
-
+const numbers = []
+setOfNumbers = new Set()
+for(i=0; i<11; i++) {
+  setOfNumbers.add(i)
+}
+console.log(setOfNumbers)
 //03.Remove an element from a set
 
 //04.Clear a set

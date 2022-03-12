7c64cf0eb3efa317fde1075d46edabd73c6e3abb
hilmidemirtas <91216830+hilmidemirtas@users.noreply.github.com>
2022-03-12 20:27:23 +0300

    Create Exercise.js

---
+++ 30-Days-Of-JavaScript-Exercise/Day_12_regular_expressions/Exercise.js
@@ -0,0 +1,4 @@
+// Level 01
+
+/* Exercise 1. Calculate the total annual income of the person from the following text.
+‘He earns 4000 euro from salary per month, 10000 euro annual bonus, 5500 euro online courses per month.’ */

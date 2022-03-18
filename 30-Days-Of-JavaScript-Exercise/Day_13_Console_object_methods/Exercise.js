7ae413ed361edeb7912c624db53cc0ffb8d39f97
hilmi.demirtas <hilmidemirtas94@gmail.com>
2022-03-19 00:10:41 +0300

    exercise 1

--- 30-Days-Of-JavaScript-Exercise/Day_13_Console_object_methods/Exercise.js
+++ 30-Days-Of-JavaScript-Exercise/Day_13_Console_object_methods/Exercise.js
@@ -3,9 +3,6 @@
 //1.Display the countries array as a table
 
 const countries = [
-    {country:"turkey"},
-    {country:"greece"},
-    {country:"turkey"},
-    {country:"greece"},
+    "turkey", "greece", "germany", "usa"
 ]
 console.table(countries)

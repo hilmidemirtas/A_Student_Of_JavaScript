c72182c0ef5b5ce3e8e7b81efb0b80d9ab7f9e69
hilmi.demirtas <hilmidemirtas94@gmail.com>
2022-03-19 00:00:47 +0300

    level 1

--- 30-Days-Of-JavaScript-Exercise/Day_13_Console_object_methods/Exercise.js
+++ 30-Days-Of-JavaScript-Exercise/Day_13_Console_object_methods/Exercise.js
@@ -1,1 +1,11 @@
-console.log("hilmi demirtaş")
+//Exercises:Level 1
+
+//1.Display the countries array as a table
+
+const countries = [
+    {country:"turkey"},
+    {country:"greece"},
+    {country:"turkey"},
+    {country:"greece"},
+]
+console.table(countries)

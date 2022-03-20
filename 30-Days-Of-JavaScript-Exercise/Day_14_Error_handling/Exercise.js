5ced2d2d72a2851592b1d5f8abe6065f1c9b62c6
hilmi.demirtas <hilmidemirtas94@gmail.com>
2022-03-20 12:29:33 +0300

    exercise 2

--- 30-Days-Of-JavaScript-Exercise/Day_14_Error_handling/Exercise.js
+++ 30-Days-Of-JavaScript-Exercise/Day_14_Error_handling/Exercise.js
@@ -3,5 +3,19 @@ try {
     let fistName = "hilmi";
     console.log(fitName);
 } catch(err) {
-    console.log("hata var")
+    console.log("error")
 }
+
+//Result
+//error
+
+/* Exercise 1 */
+try {
+    let firstName = "hilmi";
+    console.log(firstName);
+} catch(err) {
+    console.log("error")
+}
+
+//Result
+//hilmi

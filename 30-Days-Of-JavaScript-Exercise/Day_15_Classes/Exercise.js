0916b0355d11896a81c101f6ccb474096ac5dbea
hilmi.demirtas <hilmidemirtas94@gmail.com>
2022-03-22 23:34:51 +0300

    exercise 1

--- 30-Days-Of-JavaScript-Exercise/Day_15_Classes/Exercise.js
+++ 30-Days-Of-JavaScript-Exercise/Day_15_Classes/Exercise.js
@@ -1,3 +1,16 @@
 /* Exercises Level 1
+
 1.Create an Animal class. The class will have name, age, color, legs properties and create different methods */
 
+class Animal {
+    constructor(name, age, color, legs) {
+    console.log(this)
+      this.name = name
+      this.age = age
+      this.color = color
+      this.legs = legs
+    }
+}
+    const animal = new Animal('horse','7','brown', '4')
+    
+    console.log("animal")

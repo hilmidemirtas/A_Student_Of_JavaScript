4393a6fbf8dd3fbeec49633f628cd070e9539c6d
Hilmi Demirtaş <91216830+hilmidemirtas@users.noreply.github.com>
2022-04-29 23:33:43 +0300

    Update 09_day1_exercise_variables_single.js

--- 30-Days-Of-JavaScript-Exercise/Day_01/09_day1_exercise_variables_single.js
+++ 30-Days-Of-JavaScript-Exercise/Day_01/09_day1_exercise_variables_single.js
@@ -5,10 +5,10 @@ first name, last name, marital status, country and age
 in a single line
 */
 
-let firstName = "hilmi demirtaş", lastName = "demirtaş", maritalStatus = "single", country = "Turkey", age = "28"; //single line
+let firstName = "hilmi", lastName = "demirtaş", maritalStatus = "single", country = "Turkey", age = "28"; //single line
 console.log(firstName, lastName, maritalStatus, country, age) // console
 //console.log(`${} ${} ${} ${} ${}`)
 
-hilmi demirtaş demirtaş single Turkey 28 //results
+hilmi demirtaş single Turkey 28 //results
 
 

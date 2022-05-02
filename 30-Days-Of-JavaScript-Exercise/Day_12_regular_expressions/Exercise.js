6ecf250f094b641add9f123ecfb15cdb1479cbc8
Hilmi Demirtaş <91216830+hilmidemirtas@users.noreply.github.com>
2022-05-02 22:52:14 +0300

    Update Exercise.js

--- 30-Days-Of-JavaScript-Exercise/Day_12_regular_expressions/Exercise.js
+++ 30-Days-Of-JavaScript-Exercise/Day_12_regular_expressions/Exercise.js
@@ -17,7 +17,7 @@
 
 /* Exercise 2. The position of some particles on the horizontal x-axis -12, -4, -3 and -1 in the negative direction,
  0 at origin, 4 and 8 in the positive direction. Extract these numbers and find the distance between the two furthest particles. */
-
+  // result
   const pattern = /\d+/g
   const axisx = [-12, -4, -3, -1]
   const axisy= [4, 8,]
